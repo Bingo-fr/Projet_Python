0d928f72788a805707ddfb000140a69d3b1e3526
Victor RODRIGUES <rodrigues.victor4789@gmail.com>
2025-02-14 09:31:53 +0100

    Test Version

---
+++ import json.py
@@ -0,0 +1,36 @@
+import json
+
+class Jeux:
+    def __init__(self, nom, tag, image, detail):
+        self.nom = nom
+        self.tag = tag
+        self.image = image
+        self.detail = detail
+
+class Notation:
+    def __init__(self, commentaire, note):
+        self.commentaire = commentaire
+        self.note = note
+
+class Biblibiothèque:
+    def __init__(self, nom, nbr_sauvegarde, isfavorite):
+        self.nom = nom
+        self.nbr_sauvegarde = nbr_sauvegarde
+        self.isfavorite = isfavorite
+
+
+while (True):
+    print("1.Créer un commentaire et laisser une note")
+    print("2.Lister un commentaire sur un jeu")
+    print("3.Afficher la note d'un commentaire")
+    print("4.Répondre au commentaire")
+    choice = input ("Votre choix :")
+    if (choice == "1"):
+        with open("jeux.json", "r") as file:
+            library=json.load(file)
+        library.append(jeux)
+        for index, jeux in enumerate(library):
+            print(str(index) + "," + jeux["nom"])
+        choicejeux = int(input("Veuillez donnez le chiffre : "))
+        jeux = library[choicejeux] 
+        with open
